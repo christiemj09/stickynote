740cdaea7f531ecae80c2e81e8efd3a4d5437755
Matt Christie <christiemj09@gmail.com>
2018-03-16 10:57:39 -0500

    Project skeleton.

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""
+Install stickynote.
+"""
+
+def main():
+	try:
+		from setuptools import setup
+	except ImportError:
+		from distutils.core import setup
+
+	config = {
+		'description': 'Tag objects (tables, columns, others) in a PostgreSQL database.',
+		'author': 'Matt Christie',
+		'download_url': 'https://github.com/christiemj09/stickynote.git',
+		'author_email': 'christiemj09@gmail.com',
+		'version': '0.1',
+		'install_requires': [],
+		'packages': ['stickynote'],
+		'scripts': [],
+		'entry_points': {
+		    'console_scripts': [],
+		},
+		'name': 'stickynote',
+	}
+
+	setup(**config)	
+
+if __name__ == '__main__':
+	main()
+
